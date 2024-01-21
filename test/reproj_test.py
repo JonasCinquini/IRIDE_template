abd7232af3273cb751295dcc367d9f4fc011f577
Enrico Ciracì <eciraci@uci.edu>
2024-01-21 17:16:20 +0100

    reproj_test.py - unit test for generate_grid.reproject_geometry added.

---
+++ test/reproj_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+""" Unit tests for the reproject_geometry function. """
+import pytest
+from shapely.geometry import Point, Polygon
+from generate_grid import reproject_geometry
+
+@pytest.fixture
+def example_polygon():
+    """Return a square polygon."""
+    return Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+
+@pytest.fixture
+def example_point():
+    """Return a point in the center of the square polygon."""
+    return Point(0.5, 0.5)
+
+
+def test_reproject_geometry_polygon(example_polygon):
+    """Test the reproject_geometry function with a polygon."""
+    result \
+        = reproject_geometry(example_polygon, 4326, 3857)
+    assert isinstance(result, Polygon)
+
+
+def test_reproject_geometry_point(example_point):
+    """Test the reproject_geometry function with a point."""
+    result \
+        = reproject_geometry(example_point,4326, 3857)
+    assert isinstance(result, Point)
